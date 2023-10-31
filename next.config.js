02b53e23cb763c51268093c2563e2956c65d6aae
William Muli <willi.wambu@gmail.com>
2023-10-31 14:05:35 +0300

    Added unsplash domain for use in next/image component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  env: {
+    UNSPLASH_KEY: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY,
+  },
+}
 
 module.exports = nextConfig
